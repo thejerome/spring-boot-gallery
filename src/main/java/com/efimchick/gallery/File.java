cfd7d6fae1cc5c8cbb3f6402c20d56e515ca689a
thejerome <thejerome@mail.ru>
2017-10-13 17:10:30 +0300

    File implements Identifiable

--- src/main/java/com/efimchick/gallery/File.java
+++ src/main/java/com/efimchick/gallery/File.java
@@ -1,11 +1,13 @@
 package com.efimchick.gallery;
 
+import org.springframework.hateoas.Identifiable;
+
 import java.nio.file.Path;
 
 /**
  * Created by Evgenii_Efimchik on 10-Oct-17.
  */
-public interface File {
+public interface File extends Identifiable<String> {
     String getName();
 
     String getFullName();
